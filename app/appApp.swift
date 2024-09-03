45f4fc1c097199db3aa80e3bc5397abb8c29340b
Miri MR <422088820@pcpuma.acatlan.unam.mx>
2024-09-03 14:31:10 -0600

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by CEDAM09 on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
